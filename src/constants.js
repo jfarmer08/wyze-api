8b829620524ad9f86a66a45d40921fea4e4b1048
Farmer <allenfarmer1@gmail.com>
2024-08-19 13:29:25 -0500

    Update constants.js
    Semicolons: Added semicolons where missing to maintain consistency.
    Comment Consistency: Improved comments to be more descriptive and consistent.
    Template Literals: Used a template literal for userAgent to concatenate packageVersion more clearly.

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
-const packageVersion = require('../package.json').version
+const packageVersion = require('../package.json').version;
 
 module.exports = Object.freeze({
-  // Crypto Secrets
+  // Crypto Secrets (Required for device communication)
   fordAppKey: "275965684684dbdaf29a0ed9", // Required for Locks
   fordAppSecret: "4deekof1ba311c5c33a9cb8e12787e8c", // Required for Locks
   oliveSigningSecret: "wyze_app_secret_key_132", // Required for the thermostat
-  oliveAppId: "9319141212m2ik", //  Required for the thermostat
-  appInfo: "wyze_android_2.19.14", // Required for the thermostat
+  oliveAppId: "9319141212m2ik", // Required for the thermostat
 
-  // App emulation constants
+  // Application Information (for emulation)
+  appInfo: "wyze_android_2.19.14", // Required for the thermostat
   phoneId: "wyze_developer_api",
   appName: "com.hualai.WyzeCam",
   appVer: "wyze_developer_api",
@@ -16,9 +16,9 @@ module.exports = Object.freeze({
   sc: "wyze_developer_api",
   sv: "wyze_developer_api",
   authApiKey: "WMXHYf79Nr5gIlt3r0r7p9Tcw5bvs6BB4U8O8nGJ",
-  userAgent: "unofficial-wyze-api/" + packageVersion,
+  userAgent: `unofficial-wyze-api/${packageVersion}`,
 
-  //URLs
+  // Base URLs for API requests
   authBaseUrl: "https://auth-prod.api.wyze.com",
   apiBaseUrl: "https://api.wyzecam.com",
 });
